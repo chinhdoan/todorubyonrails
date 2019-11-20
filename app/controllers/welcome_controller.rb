22cacefe064e2518bd3eccbf28f23789c260766b
janson9x <missvnanhquy@gmail.com>
2019-11-21 00:10:38 +0700

    dont

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,29 +7,5 @@ class WelcomeController < ApplicationController
     @users = User.all
   end
 
-  # GET /todos/1
-  # GET /todos/1.json
-  def show
-  end
-
-  # GET /todos/new
-  def new
-    @user = Users.new
-
-  end
-
-  # GET /todos/1/edit
-  def edit
-  end
-
-  # POST /todos
-  # POST /todos.json
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
-  end
+ 
 end
